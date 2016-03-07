fa9eab8424757fc65b39d2e6e981f73c6fb5d493
James Cool <jeffreysbrother@gmail.com>
2016-03-07 10:39:59 -0800

    fix date function

--- page-about.php
+++ page-about.php
@@ -6,7 +6,7 @@
     <div class="col-md-10 col-md-offset-1 last-paragraph about">
       <img src="/wp-content/themes/smear/img/compressed/james_face_edit_small.jpg" width="180" height="184" alt="photo of james" class="img-thumbnail james">
 
-      <p class="text-muted">My name is James. I make music, think about things, and ignore certain other things. I forgot how to do calculus approximately <?php echo 'Y'-2002; ?> years ago and no longer crochet, dance, or golf. I watch a lot of horror films, but that's unimportant. </p>
+      <p class="text-muted">My name is James. I make music, think about things, and ignore certain other things. I forgot how to do calculus approximately <?php echo date('Y')-2002; ?> years ago and no longer crochet, dance, or golf. I watch a lot of horror films, but that's unimportant. </p>
 
       <!-- <p class="text-muted">In terms of programming, I am focusing primarily on web development. This site was built using Bootstrap, PHP, Grunt, and a bit of Sass. I've also utilized an image gallery by <a href="https://github.com/blueimp" target="_blank">Sebastian Tschan</a>, and some icon fonts by <a href="http://fortawesome.github.io/Font-Awesome/" target="_blank">Dave Gandy</a>. After publishing the site, and after realizing the utility of a database-driven blog, I decided to convert the existing layout into a WordPress theme. I'm currently using a number of online resources, including <a href="https://teamtreehouse.com/" target="_blank">Treehouse</a> to learn about WordPress and related technologies.</p> -->
 
