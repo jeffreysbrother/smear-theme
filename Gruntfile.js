a5be4ed3c2676f8584403a1336c1f9ddfbe1a5af
James Cool <jeffreysbrother@gmail.com>
2016-03-08 14:53:23 -0800

    remove compression from grunt sass task

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,9 +12,6 @@ module.exports = function(grunt) {
     },
     watch: {
       sass: {
-        options: {
-          style: 'compressed'
-        },
         files: ['sass/**/*.scss'],
         tasks: ['sass']
       }
