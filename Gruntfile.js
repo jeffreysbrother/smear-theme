0620a8891df709da646542758a675e7f4b40f849
James Cool <jeffreysbrother@gmail.com>
2016-03-08 14:48:52 -0800

    updated gruntfile but compression doesn't seem to work

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,9 @@ module.exports = function(grunt) {
     },
     watch: {
       sass: {
+        options: {
+          style: 'compressed'
+        },
         files: ['sass/**/*.scss'],
         tasks: ['sass']
       }
