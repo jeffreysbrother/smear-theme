09da720d7c6935b4449d1cea11fc95d56dd24eb0
James Cool <jeffreysbrother@gmail.com>
2016-03-08 12:02:55 -0800

    removed aesthetics section and associated rules

--- page-music.php
+++ page-music.php
@@ -49,45 +49,6 @@
           <h5 class="soundcloud">...MORE ON <a href="https://soundcloud.com/jeffreysbrother">SOUNDCLOUD</a>
           </h5>
 
-
-
-<!-- button -->
-<!-- <br>
-<button type="button" class="btn btn-default aes-button" data-toggle="modal" data-target="#Modal2" data-whatever="Why I don't trust aestheticians">Why I don't trust aestheticians</button> -->
-
-
-
-<!-- modal for bottom aesthetics button -->
-<?php /*
-<div class="modal fade" id="Modal2" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="exampleModalLabel">Why I don't trust aestheticians</h4>
-      </div>
-      <div class="modal-body">
-
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-          <?php the_content(); ?>
-
-        <?php endwhile; else : ?>
-          <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-        <?php endif; ?>
-
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-      </div>
-    </div>
-  </div>
-</div>
-
-*/?>
-
-
-
     </div>
 
     <div class="spacer"></div>
