c98a525584761bf0ed24c433dceac2e6801776de
James Cool <jeffreysbrother@gmail.com>
2016-03-07 16:34:32 -0800

    cleaned up commented-out code on photos page

--- page-photos.php
+++ page-photos.php
@@ -104,9 +104,7 @@
           <a href="/wp-content/themes/smear/img/compressed/mountain_under_copy.jpg" title="large" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/mountain_under-copy-th.jpg" width="150" height="100" alt="a large hill/mountain thing in Yosemite">
           </a>
-          <!-- <a href="/wp-content/themes/smear/img/compressed/pink_girl_telescope_copy.jpg" title="young girl" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/pink_girl_telescope-copy-th.jpg" width="150" height="100" alt="a young girl looking though a telescope">
-          </a> -->
+
           <a href="/wp-content/themes/smear/img/compressed/selfie_copy.jpg" title="selfie" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/selfie-copy-th.jpg" width="150" height="100" alt="a young man taking a selfie">
           </a>
@@ -116,9 +114,7 @@
           <a href="/wp-content/themes/smear/img/compressed/beach_bridge_copy.jpg" title="bridge" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/beach_bridge-copy-th.jpg" width="150" height="100" alt="bridge">
           </a>
-          <!-- <a href="/wp-content/themes/smear/img/compressed/bear_box_copy.jpg" title="bear box" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/bear_box-copy-th.jpg" width="150" height="100" alt="bear box">
-          </a> -->
+
           <a href="/wp-content/themes/smear/img/compressed/blue_green_copy.jpg" title="blue and green" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/blue_green-copy-th.jpg" width="150" height="100" alt="a hill or mountain or something in Yosemite">
           </a>
@@ -135,47 +131,24 @@
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/color_water-copy-th.jpg" width="150" height="100" alt="blue and green reflections in the water">
           </a>
 
-
           <!-- YOSEMITE END -->
 
-          <!-- <a href="/wp-content/themes/smear/img/compressed/balloons.jpg" title="old, dirty balloons" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/balloons-th.jpg" width="150" height="100" alt="old, dirty balloons on a dreary day at the Bolsa Chica Ecological Reserve">
-          </a> -->
-          <!-- <a href="/wp-content/themes/smear/img/compressed/dark_tree.jpg" title="a dreary tree" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/dark_tree-th.jpg" width="150" height="100" alt="a sad-looking tree by a small body of water">
-          </a> -->
           <a href="/wp-content/themes/smear/img/compressed/fish.jpg" title="dead fish" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/fish-th.jpg" width="150" height="100" alt="a fish head on a fence at the Bolsa Chica Ecological Reserve in Huntington Beach, CA">
           </a>
 
-          <!-- <a href="/wp-content/themes/smear/img/compressed/flower_plant.jpg" title="flower" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/flower_plant-th.jpg" width="150" height="100" alt="flower of an unknown type">
-          </a> -->
           <a href="/wp-content/themes/smear/img/compressed/green_shit.jpg" title="green shit" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/green_shit-th.jpg" width="150" height="100" alt="green shit and a bit of trash in the water">
           </a>
-          <!-- <a href="/wp-content/themes/smear/img/compressed/murk.jpg" title="murk" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/murk-th.jpg" width="150" height="100" alt="murky water">
-          </a> -->
-          <!-- <a href="/wp-content/themes/smear/img/compressed/overcast_turn.jpg" title="overcast" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/overcast_turn-th.jpg" width="150" height="100" alt="winding body of water on an overcast day">
-          </a> -->
 
           <a href="/wp-content/themes/smear/img/compressed/symmetry_water.jpg" title="water" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/symmetry_water-th.jpg" width="150" height="100" alt="a depressing shot of a body of water in Southern California">
           </a>
-          <!-- <a href="/wp-content/themes/smear/img/compressed/trash.jpg" title="trash on rocks" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/trash-th.jpg" width="150" height="100" alt="trash on some rocks next to a stick">
-          </a> -->
-          <!-- <a href="/wp-content/themes/smear/img/compressed/water_winding.jpg" title="water winding" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/water_winding-th.jpg" width="150" height="100" alt="water winding on a sunny day">
-          </a> -->
+
           <a href="/wp-content/themes/smear/img/compressed/yellow.jpg" title="yellow thing in the water" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/yellow-th.jpg" width="150" height="100" alt="yellow thing in the water">
           </a>
-          <!-- <a href="/wp-content/themes/smear/img/compressed/clouds.jpg" title="sky with clouds" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/clouds-th.jpg" width="150" height="100" alt="sky with clouds in Malibu, CA">
-          </a> -->
+
           <a href="/wp-content/themes/smear/img/compressed/clouds-2.jpg" title="sky, clouds, ocean" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/clouds-2-th.jpg" width="150" height="100" alt="sky, clouds, and ocean at Point Mugu in Malibu, CA">
           </a>
@@ -183,33 +156,19 @@
           <a href="/wp-content/themes/smear/img/compressed/numbers.jpg" title="numbers" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/numbers-th.jpg" width="150" height="100" alt="numbers on a boat in San Diego">
           </a>
-          <!-- <a href="/wp-content/themes/smear/img/compressed/building_perspective.jpg" title="glass building" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/building_perspective-th.jpg" width="150" height="100" alt="glass building reflecting the sky in San Diego">
-          </a>
-          <a href="/wp-content/themes/smear/img/compressed/building.jpg" title="building" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/building-th.jpg" width="150" height="100" alt="a building in San Diego">
-          </a> -->
-          <!-- <a href="/wp-content/themes/smear/img/compressed/formation.jpg" title="formation" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/formation-th.jpg" width="150" height="100" alt="some kind of formation on the beach at Sunset Cliffs in San Diego, CA">
-          </a> -->
+
           <a href="/wp-content/themes/smear/img/compressed/distortion_1.jpg" title="distortion 1" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/distortion_1-th.jpg" width="150" height="100" alt="distorted buildings reflected in SD">
           </a>
-          <!-- <a href="/wp-content/themes/smear/img/compressed/distortion_2.jpg" title="distortion 2" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/distortion_2-th.jpg" width="150" height="100" alt="another distorted building in SD">
-          </a> -->
+
           <a href="/wp-content/themes/smear/img/compressed/four_birds_water.jpg" title="four birds on the water" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/four_birds_water-th.jpg" width="150" height="100" alt="four birds flying near the water in San Diego">
           </a>
-          <!-- <a href="/wp-content/themes/smear/img/compressed/garage_door.jpg" title="garage door" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/garage_door-th.jpg" width="150" height="100" alt="a unique garage door in SD">
-          </a> -->
+
           <a href="/wp-content/themes/smear/img/compressed/growth.jpg" title="growth" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/growth-th.jpg" width="150" height="100" alt="some kind of plant on the coast near Sunset Cliffs">
           </a>
-          <!-- <a href="/wp-content/themes/smear/img/compressed/phone.jpg" title="phone" data-gallery>
-              <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/phone-th.jpg" width="150" height="100" alt="a dirty payphone">
-          </a> -->
+
           <a href="/wp-content/themes/smear/img/compressed/sculpted_water.jpg" title="some water" data-gallery>
               <img class="smallish" src="/wp-content/themes/smear/img/compressed/thumbs/sculpted_water-th.jpg" width="150" height="100" alt="water, coast, and a bit of plant life in San diego">
           </a>
