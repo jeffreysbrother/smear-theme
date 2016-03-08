587eb2e40047424116aaab4e783e53a92f640b76
James Cool <jeffreysbrother@gmail.com>
2016-03-08 12:08:32 -0800

    updated ambiguous class called james

--- page-about.php
+++ page-about.php
@@ -4,11 +4,9 @@
 <div class="container">
   <div class="row">
     <div class="col-md-10 col-md-offset-1 last-paragraph about">
-      <img src="/wp-content/themes/smear/img/compressed/james_face_edit_small.jpg" width="180" height="184" alt="photo of james" class="img-thumbnail james">
+      <img src="/wp-content/themes/smear/img/compressed/james_face_edit_small.jpg" width="180" height="184" alt="photo of james" class="img-thumbnail james-img">
 
       <p class="text-muted">My name is James. I make music, think about things, and ignore certain other things. I forgot how to do calculus approximately <?php echo date('Y')-2002; ?> years ago and no longer crochet, dance, or golf. I watch a lot of horror films, but that's unimportant. </p>
-
-      <!-- <p class="text-muted">In terms of programming, I am focusing primarily on web development. This site was built using Bootstrap, PHP, Grunt, and a bit of Sass. I've also utilized an image gallery by <a href="https://github.com/blueimp" target="_blank">Sebastian Tschan</a>, and some icon fonts by <a href="http://fortawesome.github.io/Font-Awesome/" target="_blank">Dave Gandy</a>. After publishing the site, and after realizing the utility of a database-driven blog, I decided to convert the existing layout into a WordPress theme. I'm currently using a number of online resources, including <a href="https://teamtreehouse.com/" target="_blank">Treehouse</a> to learn about WordPress and related technologies.</p> -->
 
       <p class="text-muted">In philosophy, I am interested in belief, justification, and logic. I occasionally find myself fiddling with an epistemological model that treats truth as subjective evaluation of accuracy, belief as a suspicion of this accuracy, and knowledge as nothing beyond either a feeling of certainty or an expression of a retroactive evaluation (classifying some <em>prior</em> judgment as veridical after obtaining confirmation). My general outlook is empiricist, nominalistic, and fallibilistic in nature. Abstract entities and moral laws, I think, are genuine fictions. Belief appears involuntary, determinism false. There are judgments, emotional responses, and actions; a lot of silliness comes from ignoring the differences between them and by failing to grasp the difference between practical and theoretical reasoning.</p>
 
