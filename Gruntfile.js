5706470296cc9b212d8094473bbcb01f154c805c
James Cool <jeffreysbrother@gmail.com>
2016-03-08 10:45:56 -0800

    added grunt, watch, sass...sass directory with modules, and .gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'style.css' : 'sass/modules/blog.scss'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: ['sass/modules/*.scss'],
+        tasks: ['sass']
+      }
+    }
+
+  });
+
+  // Load the plugins
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['sass', 'watch']);
+
+};
