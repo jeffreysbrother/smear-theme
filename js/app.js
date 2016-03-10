d8e202ed1420ee284f76c9cb60ca99c21db66684
James Cool <jeffreysbrother@gmail.com>
2016-03-09 21:49:00 -0800

    testing new js for html5 audio

--- js/app.js
+++ js/app.js
@@ -2,10 +2,20 @@
   jQuery(document).ready(function($) {
 
       // the script below allows only one audio track to play at a given time
-        $("audio").on("play", function() {
-          $("audio").not(this).each(function(index, audio) {
-              audio.pause();
-          });
+        // $("audio").on("play", function() {
+        //   $("audio").not(this).each(function(index, audio) {
+        //       audio.pause();
+        //   });
+        // });
+
+        $('audio').click(function() {
+          if (this.paused == false) {
+              this.pause();
+              alert('music paused');
+          } else {
+              this.play();
+              alert('music playing');
+          }
         });
 
   });
