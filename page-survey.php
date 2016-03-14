e77bc86f3766a1d8588456b43419c75c68a4396c
James Cool <jeffreysbrother@gmail.com>
2016-03-14 14:18:42 -0700

    change to the_content

--- page-survey.php
+++ page-survey.php
@@ -6,7 +6,7 @@
 
       <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <?php get_the_content(); ?>
+        <?php the_content(); ?>
 
       <?php endwhile; else : ?>
         <p><?php _e( 'Sorry, there is nothing here.' ); ?></p>
