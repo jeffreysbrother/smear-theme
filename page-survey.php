9600e3a057d1c6fd14c02251bfab78c3051c7f3b
James Cool <jeffreysbrother@gmail.com>
2016-03-14 14:06:24 -0700

    add page-survey

---
+++ page-survey.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="row">
+    <div class="lg-col-12">
+
+      <?php get_the_content(); ?>
+
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
