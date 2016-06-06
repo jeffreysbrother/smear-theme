5253cc6c768958cb114314f2972ca6cbc09126d4
James Cool <jeffreysbrother@gmail.com>
2016-06-06 13:08:20 -0700

    add snippet to functions php to make jquery load asynchronously

--- functions.php
+++ functions.php
@@ -43,3 +43,15 @@
     wp_enqueue_script( 'app-js', get_template_directory_uri() . '/js/app.js', array('jquery'), '', true );
   }
   add_action( 'wp_enqueue_scripts', 'jb_theme_js' );
+
+
+  // Defer jQuery Parsing using the HTML5 defer property
+  if (!(is_admin() )) {
+      function defer_parsing_of_js ( $url ) {
+          if ( FALSE === strpos( $url, '.js' ) ) return $url;
+          if ( strpos( $url, 'jquery.min.js' ) ) return $url;
+          // return "$url' defer ";
+          return "$url' defer onload='";
+      }
+      add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
+  }
