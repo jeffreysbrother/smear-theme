be5a1ed750857c40854671d76181215e918ad55a
James Cool <jeffreysbrother@gmail.com>
2016-06-06 11:54:35 -0700

    trivial change to test deploybot

--- functions.php
+++ functions.php
@@ -19,7 +19,6 @@
     wp_enqueue_style( 'font-awesome', 'http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
     wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'google-font', 'https://fonts.googleapis.com/css?family=Oswald:400,300,700' );
-
   }
   add_action( 'wp_enqueue_scripts', 'jb_theme_styles' );
 
