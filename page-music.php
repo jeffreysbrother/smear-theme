e88ab28a6e9ad5c3cb7cb4c3f44200f577f43177
James Cool <jeffreysbrother@gmail.com>
2016-06-07 14:06:15 -0700

    add no conflict wrapper to script

--- page-music.php
+++ page-music.php
@@ -57,12 +57,15 @@
 </div>
 
 <script type="text/javascript">
+//noconflict wrapper:
+  jQuery(document).ready(function($) {
       // the script below allows only one audio track to play at a given time
         $("audio").on("play", function() {
           $("audio").not(this).each(function(index, audio) {
               audio.pause();
           });
         });
+  });
 </script>
 
 
