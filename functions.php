7eb95d53c4f4ecb8d0e4cf7cb7854ee01593c1d8
James Cool <jeffreysbrother@gmail.com>
2016-06-14 23:17:33 -0700

    try to update font-awesome js

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@
     wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css' );
     wp_enqueue_style( 'blueimp-css', '//blueimp.github.io/Gallery/css/blueimp-gallery.min.css' );
     wp_enqueue_style( 'gallery', get_template_directory_uri() . '/css/bootstrap-image-gallery.min.css' );
-    wp_enqueue_style( 'font-awesome', 'http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
+    // wp_enqueue_style( 'font-awesome', 'http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' );
     wp_enqueue_style( 'style', get_template_directory_uri() . '/style.css' );
     // wp_enqueue_style( 'google-font', 'https://fonts.googleapis.com/css?family=Oswald:400,300,700' );
   }
@@ -40,6 +40,7 @@
     wp_enqueue_script( 'image-gallery-js', get_template_directory_uri() . '/js/bootstrap-image-gallery.min.js', array('jquery'), '', true );
     wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '', true );
     wp_enqueue_script( 'gist', get_template_directory_uri() . '/js/gist-embed.min.js', array('jquery'), '', true );
+    wp_enqueue_script( 'fontawesome', get_template_directory_uri() . 'https://use.fontawesome.com/1dfd33ab68.js', array('jquery'), '', true );
     // wp_enqueue_script( 'app-js', get_template_directory_uri() . '/js/app.js', array('jquery'), '', true );
   }
   add_action( 'wp_enqueue_scripts', 'jb_theme_js' );
