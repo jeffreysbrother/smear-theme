dc2eb39c999175fff5bbe417390e3133a2e65565
James Cool <jeffreysbrother@gmail.com>
2016-08-16 11:03:43 -0700

    trying to add custom field to about page

--- page-about.php
+++ page-about.php
@@ -11,6 +11,12 @@
       <p class="text-muted">In philosophy, I am interested in belief, justification, and logic. I occasionally find myself fiddling with an epistemological model that treats truth as subjective evaluation of accuracy, belief as a suspicion of this accuracy, and knowledge as nothing beyond either a feeling of certainty or an expression of a retroactive evaluation (classifying some <em>prior</em> judgment as veridical after obtaining confirmation). My general outlook is empiricist, nominalistic, and fallibilistic in nature. Abstract entities and moral laws, I think, are genuine fictions. Belief appears involuntary, determinism false. There are judgments, emotional responses, and actions; a lot of silliness comes from ignoring the differences between them and by failing to grasp the difference between practical and theoretical reasoning.</p>
 
       <p class="text-muted">One thing I'd really like to do is bring attention to certain widespread misconceptions regarding the nature of philosophical inquiry. Individuals from neighboring disciplines have only a pathetic grasp of what it is; people actively engaged in its study often ignore or fail to see the emptiness of certain questions. Common explanations concerning, for example, the relationship between science and philosophy and the purported "unanswerable" nature of philosophical questions are hasty and malformed. However, certain things like beer, JavaScript, and music will likely prevent me from doing anything useful with this information.</p>
+
+      <?php $meta_value = get_post_meta($post->ID, 'something', true);
+        if (!empty($meta_value)) {
+          echo '<p>' . $meta_value . '</p>';
+        } ?>
+
     </div>
   </div>
 
