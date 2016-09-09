9e6ce11811387ca901f1c1116d73c7aad767780d
James Cool <jeffreysbrother@gmail.com>
2016-09-09 08:58:32 -0700

    add Simpla script

--- header.php
+++ header.php
@@ -14,6 +14,8 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script src="https://app.simpla.io"></script>
+    <script>Simpla('Bk93sLxn');</script>
   </head>
 
 <body <?php body_class(); ?> onload="init();">
