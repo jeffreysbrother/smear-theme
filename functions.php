921759c971742c6cf363d55977bbaac91eb83bd1
James Cool <jeffreysbrother@gmail.com>
2016-09-22 13:34:28 -0700

    WooCommerce stuff

--- functions.php
+++ functions.php
@@ -41,3 +41,26 @@
     wp_enqueue_script( 'gist', get_template_directory_uri() . '/js/gist-embed.min.js', array('jquery'), '', true );
   }
   add_action( 'wp_enqueue_scripts', 'jb_theme_js' );
+
+
+// just in case WooCommerce screws up the shop, single product, and taxonomy pages
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+function my_theme_wrapper_start() {
+  echo '<div class="container">';
+  echo '<div class="row">';
+  echo '<div class="lg-col-12">'
+}
+function my_theme_wrapper_end() {
+  echo '</div>';
+  echo '</div>';
+  echo '</div>';
+}
+
+// declare WooCommerce support
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
